0b072961e410957a0f10dc29a8f6eb0ef597ded3
hawks890 <65943409+hawks890@users.noreply.github.com>
2021-04-14 06:04:15 -0700

    Delete incver.js

--- bin/veris_webapp/incver.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-
-const metaFileName = './assets/meta.json'
-const meta = require(metaFileName)
-
-meta.build += 1
-
-fs.writeFile(metaFileName, JSON.stringify(meta), 'utf8', () => console.log(`Updating build number to: ${meta.build}`))
